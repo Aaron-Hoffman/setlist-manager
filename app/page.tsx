f2875af535d778a8d536c5786a8b2fead4680f22
Aaron Hoffman <aahoffm@gmail.com>
2024-09-05 11:35:18 -0400

    Write and fetch from database

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,25 @@
-'use client';
-
 import AddSongForm from "@/components/AddSongForm";
 import SongList from "@/components/SongList";
 import Song from "@/types/Song";
-import { useState } from "react";
-import { sampleSize } from "lodash";
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+export default async function Home() {
+  const songs = await prisma.song.findMany()
 
-export default function Home() {
-  const [songs, setSongs] = useState([]);
-  const [setlist, setSetlist] = useState([]);
-
-  const addSong = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target.form);
-    const song: Song = {
-      title: formData.get('title'),
-      key: formData.get('key')
-    }
-
-    // await prisma.song.create({
-    //   data: {
-    //    ...song
-    //   },
-    // })
-  
-
-    return setSongs([...songs, song])
-  }
-
-  const createSetlist = () => {
-    const setlist = sampleSize(songs, 2);
-    return setSetlist(setlist);
-  }
+  // const createSetlist = () => {
+  //   const setlist = sampleSize(songs, 2);
+  //   return setSetlist(setlist);
+  // }
  
   return (
     <main className="flex flex-row items-start justify-around p-24">
-      <AddSongForm addSongHandler={addSong}/>
+      <AddSongForm />
       <SongList songList={songs}/>
-      <button onClick={createSetlist}>Create Setlist</button>
-      <h2>SETLIST</h2>
-      <SongList songList={setlist} />
+      {/* <button onClick={createSetlist}>Create Setlist</button> */}
+      {/* <h2>SETLIST</h2> */}
+      {/* <SongList songList={setlist} /> */}
     </main>
   );
 }
--- components/AddSongForm.tsx
+++ components/AddSongForm.tsx
@@ -1,18 +1,35 @@
 import KEYS from "@/constants/KEYS";
+import Song from "@/types/Song";
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient();
 
-const AddSongForm = ({addSongHandler}) => {
+const AddSongForm = () => {
+    const addSong = async (formData: FormData) => {
+        'use server'
+        const song: Song = {
+          title: formData.get('title'),
+          key: formData.get('key')
+        }
+    
+        return await prisma.song.create({
+            data: {
+             ...song
+            },
+        })
+    }
+
     return (
         <div className="flex flex-col">
             <h2>Add A Song</h2>
-            <form action="">
+            <form action={addSong}>
                 <label htmlFor="title">Title</label>
                 <input type="text" name="title" id="title" placeholder="Song title here..."/>
                 <label htmlFor="key">Key</label>
                 <select name="key" id="key">
                     {KEYS.map(key => <option value={key.value} key={key.value}>{key.label}</option>)}
                 </select>
-                <button type='submit' onClick={addSongHandler}>Add Song</button>
+                <button type='submit'>Add Song</button>
             </form>
         </div>
     )
