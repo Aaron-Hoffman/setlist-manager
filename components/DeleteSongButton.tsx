2d6e0b84e7d08f307150da33233be4c60e4f3d16
Aaron Hoffman <aahoffm@gmail.com>
2024-09-08 12:58:25 -0400

    Database schema update

--- components/DeleteSongButton.tsx
+++ components/DeleteSongButton.tsx
@@ -6,13 +6,12 @@ import { revalidatePath } from 'next/cache'
 const prisma = new PrismaClient();
 
 const DeleteSongButton = ({id, deleteSongHandler}) => {
-    const deleteSong = async (id) => {
-        console.log(id)
+    const deleteSong = async () => {
         deleteSongHandler(id)
     }
 
     return (
-        <td className="border-slate-400 border-2 p-2 cursor-pointer" onClick={() => deleteSong(id)}>X</td>
+        <td className="border-slate-400 border-2 p-2 cursor-pointer" onClick={deleteSong}>X</td>
     )
 }
 
