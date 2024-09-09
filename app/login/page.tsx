cf0ad7a88f687e9db9b3a097114c42f722b9d68e
Aaron Hoffman <aahoffm@gmail.com>
2024-09-09 10:39:17 -0400

    Add logout, session retrieval and middleware skeleton

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,16 @@
-'use client'
 import LoginButton from "@/components/LoginButton";
+import LogoutButton from "@/components/LogoutButton";
+import { getServerSession } from "next-auth";
 
-const LoginPage = () => {
-  return (
-    <LoginButton />
-  );
+const LoginPage = async () => {
+    const session = await getServerSession();
+    console.log(session)
+    return (
+        <>
+            <LoginButton />
+            <LogoutButton />
+        </>
+    );
 }
 
 export default LoginPage;
