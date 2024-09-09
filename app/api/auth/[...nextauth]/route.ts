3fb79692233eddeb5ae84bc09a45053d57e77d3c
Aaron Hoffman <aahoffm@gmail.com>
2024-09-09 14:16:12 -0400

    Update prisma to be more compatible with next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,13 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import {PrismaAdapter} from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import type { NextAuthOptions } from "next-auth";
 
-const handler = NextAuth({
+const client = new PrismaClient(); 
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(client),
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -9,6 +15,8 @@ const handler = NextAuth({
       })
   ],
   secret: process.env.NEXTAUTH_SECRET as string
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
