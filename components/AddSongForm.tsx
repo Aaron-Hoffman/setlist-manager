bee74aa0e9c694467c3917416b10350a5dc90a2e
Aaron Hoffman <aahoffm@gmail.com>
2024-11-26 15:51:01 -0500

    Apply modal to song form

--- components/AddSongForm.tsx
+++ components/AddSongForm.tsx
@@ -1,31 +1,44 @@
+'use client'
+
 import KEYS from "@/constants/KEYS";
-import { revalidatePath } from 'next/cache'
-import prisma from "@/utils/db";
 import { addSong } from "@/utils/serverActions";
+import Modal from './Modal';
+import { useState } from 'react';
+import ShowModalButton from './ShowModalButton';
 
 export type AddSongFormProps = {
     bandId: number,
 }
 
 const AddSongForm = ({bandId}: AddSongFormProps) => {
+    const [ showModal, setShowModal ] = useState(false)
+    const handleSubmit = (formData: FormData) => {
+        addSong(bandId, formData)
+        setShowModal(false)
+    }
 
     return (
-        <div className="flex flex-col">
-            <h2 className="text-center text-2xl pb-5">Add A Song</h2>
-            <form action={addSong.bind(null, bandId)} className="flex flex-col p-5 border-slate-400 border-2">
-                <div className="p-5">
-                    <label htmlFor="title" className="pr-3">Title:</label>
-                    <input type="text" name="title" id="title" placeholder="Song title here..." className="rounded p-2 "/>
+        <>
+            <ShowModalButton clickHandler={setShowModal} show={showModal}/>
+            <Modal show={showModal}>
+                <div className="flex flex-col bg-white border-black border-2 p-5 rounded">
+                    <h2 className="text-center text-2xl pb-5">Add A Song</h2>
+                    <form action={handleSubmit} className="flex flex-col p-5 border-slate-400 border-2">
+                        <div className="p-5">
+                            <label htmlFor="title" className="pr-3">Title:</label>
+                            <input type="text" name="title" id="title" placeholder="Song title here..." className="rounded p-2 "/>
+                        </div>
+                        <div className="p-5">
+                            <label htmlFor="key" className="pr-3">Key:</label>
+                            <select name="key" id="key" className="rounded p-2 ">
+                                {KEYS.map(key => <option value={key.label} key={key.value}>{key.label}</option>)}
+                            </select>
+                        </div>
+                        <button className="p-5 mt-5 bg-blue-400 rounded font-bold text-lg" type='submit'>Add Song</button>
+                    </form>
                 </div>
-                <div className="p-5">
-                    <label htmlFor="key" className="pr-3">Key:</label>
-                    <select name="key" id="key" className="rounded p-2 ">
-                        {KEYS.map(key => <option value={key.label} key={key.value}>{key.label}</option>)}
-                    </select>
-                </div>
-                <button className="p-5 mt-5 bg-blue-400 rounded font-bold text-lg" type='submit'>Add Song</button>
-            </form>
-        </div>
+            </Modal>
+        </>
     )
 }
 
