437053ac2daed675fe77775420d13e168d5df9fa
Aaron Hoffman <aahoffm@gmail.com>
2024-12-17 11:55:24 -0500

    Add basic footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <div className="h-screen">
+          <Header />
+          {children}
+          <Footer />      
+        </div>
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="flex justify-between items-end px-24 py-12 bg-slate-400">
+            <p>Developed by Aaron Hoffman &#169; 2024</p>
+        </footer>    
+    )
+}
+
+export default Footer;
