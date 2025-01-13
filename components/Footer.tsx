99e6aa502229ea2b60531280b068d0a514d0e17a
Aaron Hoffman <aahoffm@gmail.com>
2025-01-13 14:11:51 -0500

    Footer css fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="flex justify-between items-end px-24 py-12 bg-slate-400">
+        <footer className="flex justify-between items-end px-24 py-12 bg-slate-400 fixed bottom-0 right-0 left-0">
             <p>Developed by Aaron Hoffman &#169; 2024</p>
         </footer>    
     )
