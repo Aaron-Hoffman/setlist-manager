b53f6facc38436ae3da06a4898353a8f283a7dc5
Aaron Hoffman <aahoffm@gmail.com>
2025-06-12 17:43:00 -0400

    Fix setlist creation issue

--- utils/createSetList.ts
+++ utils/createSetList.ts
@@ -1,8 +1,9 @@
 import { sampleSize } from "lodash";
 import { Song } from "@prisma/client";
 
-const createSetList = (repertoire: Song[], numberOfSongs: number = 1) : Song[] => {
-    return sampleSize(repertoire, numberOfSongs);
+const createSetList = (repertoire: Song[], numberOfSongs: number = 1) => {
+    const selectedSongs = sampleSize(repertoire, numberOfSongs);
+    return selectedSongs.map(song => ({ id: song.id }));
 }
 
 export default createSetList;
