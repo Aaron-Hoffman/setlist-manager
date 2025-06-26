41a4aaff978e02a0fbcedc55d6770cdd51271aed
Aaron Hoffman <aahoffm@gmail.com>
2025-06-26 17:31:50 -0400

    Allow editing songs from setlist view

--- components/SongList.tsx
+++ components/SongList.tsx
@@ -73,7 +73,7 @@ const SongList = ({songList, add, setList}: SongListProps) => {
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                 <div className="flex items-center justify-end space-x-2">
-                                    {!setList && <EditSongForm song={song} />}
+                                    <EditSongForm song={song} />
                                     {!setList && <DeleteSongButton id={song.id} />}
                                     {setList && <EditSetListButton song={song} add={add || false} setList={setList} />}
                                 </div>
